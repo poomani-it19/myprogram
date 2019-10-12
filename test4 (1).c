f787de04764597d3e415353ebc216a53811bc36c
poomani-it19 <54310549+poomani-it19@users.noreply.github.com>
2019-10-12 11:48:56 +0530

    third commit

---
+++ test4 (1).c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ int main()
+ { int num1,num2;
+ float result;
+ char ch;
+ //to store operator choice printf("Enter first number: "); scanf("%d",&num1); printf("Enter second nu$
+
+
+
+
+
